cdcba864e06cd93ded46cb795db01319dd46661e
ToropovAlexei <henschel37rus@gmail.com>
2025-09-05 19:23:22 +0300

    pg db

--- backend/db/database.py
+++ backend/db/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import declarative_base
 
 from config import settings
 
-engine = create_async_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_async_engine(settings.DATABASE_URL)
 SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
