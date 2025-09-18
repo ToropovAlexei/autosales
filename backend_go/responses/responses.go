b50d2f90c801ccbbee397011dcf5ad11ddad47ce
ToropovAlexei <henschel37rus@gmail.com>
2025-09-18 21:09:32 +0300

    responses

--- backend_go/responses/responses.go
+++ backend_go/responses/responses.go
@@ -3,9 +3,9 @@ package responses
 import "github.com/gin-gonic/gin"
 
 func ErrorResponse(c *gin.Context, statusCode int, message string) {
-	c.JSON(statusCode, gin.H{"error": message})
+	c.JSON(statusCode, gin.H{"success": false, "data": nil, "error": message})
 }
 
 func SuccessResponse(c *gin.Context, statusCode int, data interface{}) {
-	c.JSON(statusCode, data)
+	c.JSON(statusCode, gin.H{"success": true, "data": data, "error": nil})
 }
