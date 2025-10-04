0dce5c981b89d3cc0e0a7fb562f61007fe261e4b
ToropovAlexei <henschel37rus@gmail.com>
2025-10-04 21:00:36 +0300

    fix

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,11 +1,10 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded';
-import { ColorModeIconDropdown } from '@/components/ColorModeIconDropdown';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import DashboardRoundedIcon from "@mui/icons-material/DashboardRounded";
+import { ColorModeIconDropdown } from "@/components/ColorModeIconDropdown";
 
 export const NavBar = () => {
   return (
@@ -13,25 +12,25 @@ export const NavBar = () => {
       position="fixed"
       sx={{
         boxShadow: 0,
-        bgcolor: 'background.paper',
-        backgroundImage: 'none',
-        borderBottom: '1px solid',
-        borderColor: 'divider',
+        bgcolor: "background.paper",
+        backgroundImage: "none",
+        borderBottom: "1px solid",
+        borderColor: "divider",
         zIndex: (theme) => theme.zIndex.drawer + 1,
       }}
     >
       <Toolbar
         sx={{
-          height: 'var(--template-navbar-height)',
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
+          height: "var(--template-navbar-height)",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
           gap: 1,
         }}
       >
         <Stack direction="row" spacing={1} alignItems="center">
           <DashboardRoundedIcon color="primary" />
-          <Typography variant="h6" component="div">
+          <Typography variant="h6" component="div" color="text.primary">
             Админ панель
           </Typography>
         </Stack>
