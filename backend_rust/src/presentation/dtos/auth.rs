0ab049bfca044b18c27ae7b34e067ebf6d4506c1
ToropovAlexei <henschel37rus@gmail.com>
2025-12-27 22:41:21 +0300

    rs

--- backend_rust/src/presentation/dtos/auth.rs
+++ backend_rust/src/presentation/dtos/auth.rs
@@ -1,8 +1,18 @@
+use serde::{Deserialize, Serialize};
+use ts_rs::TS;
+use utoipa::{ToResponse, ToSchema};
+use uuid::Uuid;
+
+#[derive(Debug, Clone, Serialize, Deserialize, TS, ToSchema, ToResponse)]
+#[ts(export, export_to = "auth.ts", rename = "LoginStep1")]
 pub struct LoginStep1Request {
     pub login: String,
     pub password: String,
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize, TS, ToSchema, ToResponse)]
+#[ts(export, export_to = "auth.ts", rename = "LoginStep2")]
 pub struct LoginStep2Request {
+    pub temp_token: Uuid,
     pub code: String,
 }
